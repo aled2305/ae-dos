d0bd3641528a6f3d6f0d8d600cebd92c909c186a
Aled Evans <aleddevans2305@gmail.com>
2021-09-19 19:35:50 +0100

    Setup base and theme switcher for testing

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<h1>Hello World!</h1>
+<!DOCTYPE html>
+<html lang="en" dir="ltr" class="theme-light">
+  <head>
+    <meta charset="utf-8">
+    <title>AE-DOS</title>
+    <link rel="stylesheet" href="/includes/frameworks/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/includes/frameworks/fontawsome/css/all.css">
+    <link rel="stylesheet" href="/includes/css/theme-switch.css">
+    <link rel="stylesheet" href="/includes/css/master.css">
+
+    <script src="/includes/frameworks/bootstrap/js/bootstrap.min.js" charset="utf-8"></script>
+    <script src="/includes/frameworks/fontawsome/js/all.js" charset="utf-8"></script>
+    <script src="/includes/frameworks/jquery/jquery.js" charset="utf-8"></script>
+
+  </head>
+  <body>
+    <div class="container-fluid p-3">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="tile"></div>
+        </div>
+        <div class="col-lg-4">
+          <div class="tile"></div>
+        </div>
+      </div>
+    </div>
+    <!-- Theme Switcher -->
+    <div class="theme-switch">
+      <label id="switch" class="switch">
+          <input type="checkbox" onchange="toggleTheme()" id="slider">
+          <span class="slider round"></span>
+      </label>
+    </div>
+    <script src="/includes/js/theme-switch.js" charset="utf-8"></script>
+  </body>
+</html>
