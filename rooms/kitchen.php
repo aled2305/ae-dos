02598414109ed10af618f9bd9ee4d8bd66c5c7ee
Aled Evans <aleddevans2305@gmail.com>
2021-10-01 14:28:59 +0100

    Room added with lights working

---
+++ rooms/kitchen.php
@@ -0,0 +1,40 @@
+<div class="row">
+  <div class="col-lg-2">
+    <div id="light__kitchen" class="item" onclick="toggleLight('light.kitchen');" data-bs-target="#lightModal" data-element="light__kitchen" data-colour="" data-state="">
+      <div class="item-title">Kitchen</div>
+      <div class="item-icon">
+        <svg id="lamp" data-name="lamp" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 248.05 248.05">
+          <defs>
+            <radialGradient id="gradient" cx="134.51" cy="-0.09" r="350.14" gradientUnits="userSpaceOnUse">
+              <stop offset="0" stop-color="var(--color-stop)"/>
+              <stop offset="1" stop-color="var(--color-stop2)"/>
+            </radialGradient>
+            <radialGradient id="New_Gradient_Swatch_copy_12-2" cx="146.57" cy="11.97" r="350.14" xlink:href="#gradient"/>
+            <radialGradient id="New_Gradient_Swatch_copy_12-3" cx="146.57" cy="11.97" r="350.14" xlink:href="#gradient"/>
+            <radialGradient id="New_Gradient_Swatch_copy_12-4" cx="146.57" cy="11.97" r="350.14" xlink:href="#gradient"/>
+            <radialGradient id="New_Gradient_Swatch_copy_12-5" cx="146.57" cy="11.97" r="350.14" xlink:href="#gradient"/>
+            <radialGradient id="New_Gradient_Swatch_copy_12-6" cx="146.57" cy="11.97" r="350.14" xlink:href="#gradient"/>
+            <radialGradient id="New_Gradient_Swatch_copy_12-7" cx="146.57" cy="11.97" r="350.14" xlink:href="#gradient"/>
+            <radialGradient id="New_Gradient_Swatch_copy_12-8" cx="146.57" cy="11.97" r="350.14" xlink:href="#gradient"/>
+          </defs>
+          <rect class="cls-1" x="138.84" y="114.57" width="12.81" height="26.22"/>
+          <path class="cls-2" d="M108.29,132.62v20.23H121.1V129.57l39.36-9.39a14.9,14.9,0,0,0,11-18l-59.78,14.26a14.9,14.9,0,0,0-11,18Z" transform="translate(-12.06 -12.06)"/>
+          <path class="cls-3" d="M160.46,52.25a14.9,14.9,0,0,0,11-18L111.71,48.56a14.9,14.9,0,0,0-11,18Z" transform="translate(-12.06 -12.06)"/>
+          <path class="cls-4" d="M160.46,74.89a14.89,14.89,0,0,0,11-17.94L111.71,71.21a14.89,14.89,0,0,0-11,17.94Z" transform="translate(-12.06 -12.06)"/>
+          <path class="cls-5" d="M160.46,97.54a14.9,14.9,0,0,0,11-18L111.71,93.85a14.9,14.9,0,0,0-11,18Z" transform="translate(-12.06 -12.06)"/>
+          <path class="cls-6" d="M112.78,201.56a17,17,0,0,0,17,17h8.94a17,17,0,0,0,17-17h-42.9Z" transform="translate(-12.06 -12.06)"/>
+          <path class="cls-7" d="M136.08,12.06a124,124,0,1,0,124,124A124,124,0,0,0,136.08,12.06Zm0,233.89h-1.23a14,14,0,0,0,13.38-14v-11h-28v11a14,14,0,0,0,13,14,110.09,110.09,0,1,1,2.8,0Z" transform="translate(-12.06 -12.06)"/>
+          <path class="cls-8" d="M98,154.93v18.32a25.48,25.48,0,0,0,25.48,25.48h25.17a25.47,25.47,0,0,0,25.47-25.48V154.93H98Z" transform="translate(-12.06 -12.06)"/>
+        </svg>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-2">
+    <div id="light__kitchen_table" class="item" onclick="toggleLight('light.kitchen_table');" data-bs-target="#lightModal" data-element="light__kitchen_table" data-colour="" data-state="">
+      <div class="item-title">Kitchen Table</div>
+      <div class="item-icon">
+        <svg id="lamp" data-name="lamp" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 248.05 248.05"><defs><radialGradient id="New_Gradient_Swatch_copy_12" cx="134.51" cy="-0.09" r="350.14" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#f2a522"/><stop offset="1" stop-color="#eb5a32"/></radialGradient><radialGradient id="New_Gradient_Swatch_copy_12-2" cx="146.57" cy="11.97" r="350.14" xlink:href="#New_Gradient_Swatch_copy_12"/><radialGradient id="New_Gradient_Swatch_copy_12-3" cx="146.57" cy="11.97" r="350.14" xlink:href="#New_Gradient_Swatch_copy_12"/><radialGradient id="New_Gradient_Swatch_copy_12-4" cx="146.57" cy="11.97" r="350.14" xlink:href="#New_Gradient_Swatch_copy_12"/><radialGradient id="New_Gradient_Swatch_copy_12-5" cx="146.57" cy="11.97" r="350.14" xlink:href="#New_Gradient_Swatch_copy_12"/><radialGradient id="New_Gradient_Swatch_copy_12-6" cx="146.57" cy="11.97" r="350.14" xlink:href="#New_Gradient_Swatch_copy_12"/><radialGradient id="New_Gradient_Swatch_copy_12-7" cx="146.57" cy="11.97" r="350.14" xlink:href="#New_Gradient_Swatch_copy_12"/><radialGradient id="New_Gradient_Swatch_copy_12-8" cx="146.57" cy="11.97" r="350.14" xlink:href="#New_Gradient_Swatch_copy_12"/></defs><rect class="cls-1" x="138.84" y="114.57" width="12.81" height="26.22"/><path class="cls-2" d="M108.29,132.62v20.23H121.1V129.57l39.36-9.39a14.9,14.9,0,0,0,11-18l-59.78,14.26a14.9,14.9,0,0,0-11,18Z" transform="translate(-12.06 -12.06)"/><path class="cls-3" d="M160.46,52.25a14.9,14.9,0,0,0,11-18L111.71,48.56a14.9,14.9,0,0,0-11,18Z" transform="translate(-12.06 -12.06)"/><path class="cls-4" d="M160.46,74.89a14.89,14.89,0,0,0,11-17.94L111.71,71.21a14.89,14.89,0,0,0-11,17.94Z" transform="translate(-12.06 -12.06)"/><path class="cls-5" d="M160.46,97.54a14.9,14.9,0,0,0,11-18L111.71,93.85a14.9,14.9,0,0,0-11,18Z" transform="translate(-12.06 -12.06)"/><path class="cls-6" d="M112.78,201.56a17,17,0,0,0,17,17h8.94a17,17,0,0,0,17-17h-42.9Z" transform="translate(-12.06 -12.06)"/><path class="cls-7" d="M136.08,12.06a124,124,0,1,0,124,124A124,124,0,0,0,136.08,12.06Zm0,233.89h-1.23a14,14,0,0,0,13.38-14v-11h-28v11a14,14,0,0,0,13,14,110.09,110.09,0,1,1,2.8,0Z" transform="translate(-12.06 -12.06)"/><path class="cls-8" d="M98,154.93v18.32a25.48,25.48,0,0,0,25.48,25.48h25.17a25.47,25.47,0,0,0,25.47-25.48V154.93H98Z" transform="translate(-12.06 -12.06)"/></svg>
+      </div>
+    </div>
+  </div>
+</div>
